72d3b325489085758d8ccf9aebbce41ad339c4ae
Zetero <80723607+Zetero@users.noreply.github.com>
2023-01-19 20:52:28 +0300

    Added urls and views functions

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("Главная страничка")
+
+
+def group_posts(request, slug):
+    return HttpResponse(f"Груповая страничка с параметром {slug}")
