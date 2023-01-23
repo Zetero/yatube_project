8e96aad7e412e8a3af28cb936b9202f5d0aae33c
Zetero <80723607+Zetero@users.noreply.github.com>
2023-01-23 16:29:56 +0300

    Created tamplates folder and tested dev-server

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Главная страничка")
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, slug):
